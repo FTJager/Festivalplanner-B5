687959305f842fafd42ab08bd2272c434fa822b0
Kim <kimveldhoen@hotmail.com>
2020-02-03 14:38:07 +0100

    Made a popup, not perfect but its something. I tried a try-catch thingie but its not working.

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -8,8 +8,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
 
@@ -47,9 +45,9 @@ public class GUI extends Application {
         BorderPane pane = new BorderPane();
 
         //BUTTONS
-        Button newButton = new Button("new");
-        Button editButton = new Button("edit");
-        Button deleteButton = new Button("delete");
+        Button newButton = new Button("New");
+        Button editButton = new Button("Edit");
+        Button deleteButton = new Button("Delete");
         HBox buttonBox = new HBox();
         buttonBox.setSpacing(50);
         buttonBox.getChildren().addAll(newButton, editButton, deleteButton);
@@ -66,26 +64,57 @@ public class GUI extends Application {
         stage.setScene(scene);
         stage.show();
 
-        //POPUP
+        //POPUP ASSETS
         Popup popup = new Popup();
         Label artistLabel = new Label("Artist: ");
-        BorderPane popupPane = new BorderPane();
-        Stage popupStage = new Stage();
+        TextField artistField = new TextField();
+        Label popularityLabel = new Label("Popularity: ");
+        TextField popularityField = new TextField();
+        Label stageLabel = new Label("Stage:");
 
-        //Pane popupPane = new Pane();
-        popupPane.setStyle("-fx-background-color: #000000;");
-        Scene popupScene = new Scene(popupPane, 200, 200);
-        popupStage.setScene(popupScene);
+        Button doneButton = new Button("Done");
 
-        //popup.getContent().addAll(popupStage);
+        VBox labelBox = new VBox();
+        labelBox.getChildren().addAll(artistLabel, popularityLabel);
+        labelBox.setSpacing(35);
+        VBox fieldBox = new VBox();
+        fieldBox.getChildren().addAll(artistField, popularityField);
+        fieldBox.setSpacing(20);
+
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(labelBox, fieldBox);
+        hBox.setSpacing(10);
+        VBox popupVBox = new VBox();
+        popupVBox.getChildren().addAll(hBox, doneButton);
+        popupVBox.setSpacing(15);
+
+        //POPUP
+        Pane popupPane = new Pane();
+        popupPane.setStyle("-fx-background-color: #FFFFFF;");
+        popupPane.getChildren().addAll(popupVBox);
+
+        popup.getContent().addAll(popupPane);
         popup.setAutoHide(false);
-        popup.setX(200);
-        popup.setY(200);
+        popup.setX(1090/2);
+        popup.setY(920/2);
 
         newButton.setOnAction(event -> {
             if (!popup.isShowing()){
                 popup.show(stage);
             }
+        });
+
+        doneButton.setOnAction (event -> {
+            try {
+                if(popup.isShowing()) {
+                    if (artistField.getText().equals(dataStore.Artists.getArtists())) {
+                        //if everything is fine and the times do not overlap,
+                        //put that shit up on the table
+                        popup.hide();
+                    }
+                }
+            }
+            catch (//give a warning for overlappig times) // whatever i dont get it
         });
 
     }
