b68dff7c164fb97a7557c73976754ada57718ef7
Nathalie Seen <n.seen@student.avans.nl>
2020-02-03 12:49:46 +0100

    klasse stage

--- src/Data/Stage.java
+++ src/Data/Stage.java
@@ -1,4 +1,35 @@
 package Data;
 
 public class Stage {
+
+    private String stageName;
+    private int size;
+    private boolean inUse;
+
+    public Stage(String stageName, int size, boolean inUse) {
+        this.stageName = stageName;
+        this.size = size;
+        this.inUse = inUse;
+    }
+
+    public String getStageName() {
+        return stageName;
+    }
+    public void setStageName(String stageName){
+        this.stageName = stageName;
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public void setSize(int size){
+        this.size = size;
+    }
+
+    public boolean getInUse() {
+        return inUse;
+    }
+    public void setInUse(boolean inUse){
+        this.inUse = inUse;
+    }
 }
