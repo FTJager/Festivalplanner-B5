8f6d31f285dcd076a05b16ac52fcc29df7e0dc41
RemoMeijer <remcomeijer0210@gmail.com>
2020-02-06 23:24:58 +0100

    Removed old code, added comments to new code.

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -2,22 +2,11 @@ package GUI;
 
 import Data.Show;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
-
 import java.awt.*;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.RoundRectangle2D;
@@ -25,8 +14,6 @@ import java.util.ArrayList;
 
 public class GUI extends Application {
 
-    private HBox buttonBox = new HBox();
-    private TableView<TableColumn> tableView = new TableView<>();
     private ArrayList<Show> shows;
     private Stage stage;
     private Canvas canvas;
@@ -38,61 +25,20 @@ public class GUI extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        this.stage = stage;
-//        initialiseButtons();
-//        initialiseMainScene();
         this.canvas = new Canvas(800, 630);
         tableDraw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+
+        this.stage = stage;
         this.stage.setScene(new Scene(new Group(canvas)));
         this.stage.show();
         Buttoninteraction();
     }
 
-    //Initialises the main scene
-    public void initialiseMainScene(){
-        this.shows = new ArrayList<>();
-        VBox vBox = new VBox();
-        vBox.setSpacing(10);
-        vBox.getChildren().addAll(tableView);
 
-        FlowPane root = new FlowPane();
-        root.setAlignment(Pos.CENTER);
-
-        this.stage.setWidth(900);
-        this.stage.setHeight(500);
-        this.stage.setResizable(false);
-        this.stage.setTitle("Festival Agenda");
-
-        Scene scene = new Scene(root, 900, 500);
-        root.getChildren().addAll(vBox, this.buttonBox);
-
-        this.stage.setScene(scene);
-        this.stage.show();
-    }
-
-    //Makes Buttons
-    public void initialiseButtons() {
-        Button newButton = new Button("New");
-        Button editButton = new Button("Edit");
-        Button deleteButton = new Button("Delete");
-
-        this.buttonBox.setSpacing(50);
-        this.buttonBox.getChildren().addAll(newButton, editButton, deleteButton);
-
-        newButton.setOnAction(event -> {
-            NewStage newStage = new NewStage(shows);
-        });
-
-        editButton.setOnAction(event ->{
-            EditStage editStage = new EditStage();
-        });
-
-        deleteButton.setOnAction(event ->{
-            DeleteStage deleteStage = new DeleteStage();
-        });
-    }
-
+    //Initialises the whole table
     public void tableDraw(FXGraphics2D graphics) {
+
+        //Makes the top part of the table
         GeneralPath topPath = new GeneralPath();
         topPath.moveTo(0,0);
         topPath.lineTo(canvas.getWidth(), 0);
@@ -102,6 +48,7 @@ public class GUI extends Application {
         graphics.setColor(Color.getHSBColor(0, 0, 0.5f));
         graphics.fill(topPath);
 
+        //Makes the small black line at the top of the table
         GeneralPath topLine = new GeneralPath();
         topLine.moveTo(0, 57);
         topLine.lineTo(canvas.getWidth(), 57);
@@ -111,6 +58,7 @@ public class GUI extends Application {
         graphics.setColor(Color.black);
         graphics.fill(topLine);
 
+        //Makes the main table
         float beginX = 0;
         float beginY = 60;
 
@@ -120,6 +68,7 @@ public class GUI extends Application {
             tablePath.lineTo(canvas.getWidth(), beginY);
             tablePath.lineTo(canvas.getWidth(), beginY + 20);
             tablePath.lineTo(beginX, beginY + 20);
+            //Makes the table in 2 colors
             if(i % 2 == 0) {
                 graphics.setColor(Color.getHSBColor(0, 0, 0.78f));
                 graphics.fill(tablePath);
@@ -132,56 +81,40 @@ public class GUI extends Application {
             graphics.drawString(i + "", beginX + 10, beginY + 15);
             beginY += 20;
         }
+
+        //Makes the buttons "New", "Edit" and "Delete"
         graphics.setColor(Color.black);
         RoundRectangle2D newButton = new RoundRectangle2D.Double(30, 570.0, 80, 30, 8, 8);
         RoundRectangle2D editButton = new RoundRectangle2D.Double(135, 570.0, 80, 30, 8, 8);
         RoundRectangle2D deleteButton = new RoundRectangle2D.Double(240, 570.0, 80, 30, 8, 8);
+
         Font buttonFont = new Font("Arial", Font.PLAIN, 25);
         graphics.setFont(buttonFont);
         graphics.drawString("new", 45, 590);
         graphics.drawString("edit", 150, 590);
         graphics.drawString("delete", 250, 590);
+
         graphics.draw(newButton);
         graphics.draw(editButton);
         graphics.draw(deleteButton);
     }
 
+
+    //Makes the buttons intractable, remember to match the event box coordinates with the buttons coordinates if the buttons are ever moved.
     public void Buttoninteraction(){
         canvas.setOnMouseClicked(event -> {
+            //Event for "New" button
             if(event.getX() > 30 && event.getX() < 110 && event.getY() >570 && event.getY() < 600) {
                 newStage = new NewStage(shows);
             }
+            //Event for "Edit" button
             if(event.getX() > 135 && event.getX() < 215 && event.getY() > 570 && event.getY() < 600) {
                 EditStage editStage = new EditStage();
             }
+            //Event for "Delete" button.
             if(event.getX() > 240 && event.getX() < 320 && event.getY() > 570 && event.getY() < 600) {
                 DeleteStage deleteStage = new DeleteStage();
             }
         });
     }
-
-//    //Makes the table on the main scene
-//    public void tableView() {
-//        TableColumn timeColumn = new TableColumn("Time:");
-//        TableColumn mainStageColumn = new TableColumn("Main stage:");
-//        TableColumn backStageColumn = new TableColumn("Back stage:");
-//        TableColumn sideStageColumn = new TableColumn("Side stage:");
-//        TableColumn smallStageColumn = new TableColumn("Small stage:");
-//
-//        timeColumn.setPrefWidth(80);
-//        mainStageColumn.setPrefWidth(200);
-//        backStageColumn.setPrefWidth(200);
-//        sideStageColumn.setPrefWidth(200);
-//        smallStageColumn.setPrefWidth(200);
-//
-//        this.tableView.getColumns().addAll(timeColumn,
-//                mainStageColumn,
-//                backStageColumn,
-//                sideStageColumn,
-//                smallStageColumn);
-//        this.tableView.setEditable(false);
-//
-//    }
-
-
 }
