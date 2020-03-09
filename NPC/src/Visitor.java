c26742dd2cefe7f81f53a7a9e25bc8928d03aafe
Florian <floriantjager@gmail.com>
2020-03-09 10:11:44 +0100

    NPC Update, Markdown folder

--- NPC/src/Visitor.java
+++ NPC/src/Visitor.java
@@ -2,8 +2,8 @@ import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 public class Hick extends NPC {
-    public Hick(Point2D position, BufferedImage sprite) {
-        super(position, sprite);
+    public Hick(Point2D position, BufferedImage sprite, Point2D target) {
+        super(position, sprite, target);
         super.stageCollision = true;
     }
 }
